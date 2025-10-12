a13a6c807320b51a87db9173f5f552acdd821c42
Tijani Moro <tjhackx111@gmail.com>
2025-10-12 12:41:05 +0000

    initial commit

--- 5001.php
+++ 5001.php
@@ -3,3 +3,91 @@
     include ('config.php');
 
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fiaxit . Waitlist</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+
+    <link href="https://coderthemes.com/around/assets/icons/around-icons.min.css" rel="stylesheet" type="text/css">
+    <link href="https://coderthemes.com/around/assets/css/theme.min.css" rel="stylesheet" type="text/css">
+    <link href="css.css" rel="stylesheet" type="text/css">
+   
+</head>
+<body >
+    <main class="page-wrapper">
+
+        <div class="row justify-content-center">
+            <!-- Page content -->
+            <div class="col-lg-9 pt-4 pb-2 pb-sm-4">
+                <div class="d-sm-flex align-items-center mb-4">
+                    <h1 class="h2 mb-4 mb-sm-0 me-4">Wait list</h1>
+                    <div class="d-flex ms-auto">
+                        <button class="btn btn-secondary me-3 me-sm-4" type="button">Download</button>
+                    </div>
+                </div>
+                <div class="card border-0 py-1 p-md-2 p-xl-3 p-xxl-4">
+                    <div class="card-body">
+
+                        <!-- Stats -->
+                        <div class="row g-3 g-xl-4">
+                            <div class="col-md-6 col-sm-6">
+                                <div class="h-100 bg-secondary rounded-3 text-center p-4">
+                                    <h2 class="h6 pb-2 mb-1">Number</h2>
+                                    <div class="h2 text-primary mb-2">$842.00</div>
+                                        <p class="fs-sm text-body-secondary mb-0">Sales 8/1/2023 - 8/15/2023</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="h-100 bg-secondary rounded-3 text-center p-4">
+                                        <h2 class="h6 pb-2 mb-1">Visitors</h2>
+                                        <div class="h2 text-primary mb-2">$735.00</div>
+                                            <p class="fs-sm text-body-secondary mb-0">To be paid on 8/15/2023</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row g-4 py-4">
+                            <div class="h-100 border card card-body">
+                                <!-- Basic table -->
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">First Name</th>
+                                                <th scope="col">Last Name</th>
+                                                <th scope="col">Position</th>
+                                                <th scope="col">Phone</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>John</td>
+                                                <td>Doe</td>
+                                                <td>CEO, Founder</td>
+                                                <td>+3 555 68 70</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+    <script src="https://coderthemes.com/around/assets/js/theme.min.js"></script>
+
+</body>
+</html>
